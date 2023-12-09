47c385228d8f6516a10f49485dfd73085a1ad552
andre janvier omgba <janvieromgbaomgba@outlook.fr>
2023-12-10 00:46:47 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+import Profile from "./pages/profile/profile"
+import Signup from "./pages/signup/Signup"
+import Signin from "./pages/signin/Signin"
 
 
 
@@ -6,9 +12,16 @@ function App() {
 
   return (
    
-      <div className="text-3xl font-bold">
-         Welcome OMGBA JANVIER
-      </div>
+      <BrowserRouter>
+         <Routes>
+           <Route path="/" element={<Home/>} />
+           <Route path="/about" element={<About/>} />
+           <Route path="/profile" element={<Profile/>} />
+           <Route path="/signup" element={<Signup/>} />
+           <Route path="/signin" element={<Signin/>} />
+
+         </Routes>
+      </BrowserRouter>
       
     
   )
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='text-3xl font-bold'>
+      About
+    </div>
+  );
+}
+
+export default About;
